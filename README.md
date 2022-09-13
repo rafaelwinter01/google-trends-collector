7d91e54082b7e527e7b5402cea4b0200e4d60a22
Rafael Winter <rafael.winter@serpro.gov.br>
2022-09-13 12:51:34 -0700

    Initial Version

---
+++ README.md
@@ -0,0 +1,51 @@
+## 1 - Description of the Idea
+The idea here is to collect data from Google Trends between certain period of time.
+The program will collect all data using the keyword 'bitcoin' between a initial day
+(fixed as '2015-01-01') and the final day that is the day the program will run. 
+
+It was used two 3rd party libraries to finish the task of collecting:
+    - g-trends: resposible for collect the data from Google Trends
+    - objects-to-csv: resposible for write the files with the data collected
+
+Google Trends provides the periodicity of the searched data depending on the range of dates
+that is being asked for, for some months, it will be returned in records of days, for more months
+of few years, it will be returned in records of weeks, and for more years it will be returned
+in records of months.
+
+The data collected will be written to CSV files, separated by the ranges of dates that are 
+being asked for.
+
+## 2 - Amount of time spent on finishing the task
+I took two to three hours to finish the tasks. Searching for better solutions, studying the
+libraries, developing and testing.
+
+## 3 - Different ways tried to solve the problem
+At first, I thought that would be necessary some webscrapping solution. But, searching a little
+more, it was easy to find a library that could help with the development.
+
+## 4 - Reasons of using the current approach
+The library 'G-Trends' showed to be a good solution for the task, since is easy to use and provides
+enough tools for what is proposed.
+
+## 5 - Execution
+After dowload the source code, is necessary to install 'Node.js' in the computer, if is not already
+installed. Opening a command prompt at the folder of the project, executes the command:
+```
+npm install
+```
+To install the related libraries.
+
+And run the program, using the following command (also in the folder of the project):
+```
+node index.js
+```
+
+Obs.:
+Is necessary to be logged in on a Google Account and also provides read and write permissions on 
+the project folder.
+
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,116 @@
+const { ExploreTrendRequest } = require('g-trends')
+const objToCSV = require('objects-to-csv');
+
+var keyword = 'bitcoin'
+var iniDate = '2015-01-01'
+
+
+/*  
+    Google Trends provides the information daily, weekly and monthly, depending on how long
+    is the range of data that is being collected.
+*/
+
+function setDateRanges(iniDate, periodicity='weekly')  {
+    var condition = false
+    var dateRanges = []
+    var index=0
+    var finDate = new Date(Date.now())
+
+    if(periodicity == 'weekly') {
+        periodicity=12
+    } else {
+        periodicity=6
+    }
+
+    var initialDate = new Date(Date.parse(iniDate))
+    initialDate.setDate(initialDate.getDate() + 1)
+    while(!condition) {
+        let finalDate = new Date(initialDate)
+        finalDate.setMonth(finalDate.getMonth() + periodicity)
+        finalDate.setDate(finalDate.getDate() - 1)
+        
+        dateRanges[index] = {
+            initialDate: initialDate.getFullYear()+'-'+(initialDate.getMonth()+1)+'-'+initialDate.getDate(),
+            finalDate: finalDate.getFullYear()+'-'+(finalDate.getMonth()+1)+'-'+finalDate.getDate()
+        }
+    
+        initialDate = new Date(finalDate)
+        initialDate.setDate(initialDate.getDate() + 1)
+    
+        index++
+        if(finalDate > finDate) 
+            condition=true
+    }
+
+    return dateRanges
+}
+
+function collectData(keyword, dateRanges, fileName) {
+    for(let i=0; i<dateRanges.length; i++) {
+        const newExplorer = new ExploreTrendRequest()
+        newExplorer.addKeyword(keyword)
+            .between(dateRanges[i].initialDate, dateRanges[i].finalDate)
+            .download().then( file => {
+                const fileToCSV = new objToCSV(file)
+                return fileToCSV.toDisk(`./csv/${fileName}-${dateRanges[i].initialDate}_to_${dateRanges[i].finalDate}.csv`)
+            })
+    } 
+}
+
+collectData(keyword, setDateRanges(iniDate, 'daily'), 'daily')
+
+collectData(keyword, setDateRanges(iniDate), 'weekly')
+
+
+
+
+// for(let i=0; i<dateRanges.length; i++) {
+//     const newExplorerWeekly = new ExploreTrendRequest()
+//     newExplorerWeekly.addKeyword(keyword)
+//         .between(dateRanges[i].initialDate, dateRanges[i].finalDate)
+//         .download().then( file => {
+//             const fileToCSV = new objToCSV(file)
+//             return fileToCSV.toDisk(`./csv/weekly-${(dateRanges[i].initialDate).substr(0,4)}.csv`)
+//         })
+// }
+
+// /*
+//     Second, information will be collected daily, searching each six months
+// */
+// const months = 6
+
+// initialDate = new Date(Date.parse(iniDate))
+// initialDate.setDate(initialDate.getDate() + 1)
+
+// condition = false
+// dateRanges = []
+// index=0
+
+// while(!condition) {
+//     let finalDate = new Date(initialDate)
+//     finalDate.setMonth(finalDate.getMonth() + months)
+//     finalDate.setDate(finalDate.getDate() - 1)
+    
+//     dateRanges[index] = {
+//         initialDate: initialDate.getFullYear()+'-'+(initialDate.getMonth()+1)+'-'+initialDate.getDate(),
+//         finalDate: finalDate.getFullYear()+'-'+(finalDate.getMonth()+1)+'-'+finalDate.getDate()
+//     }
+
+//     initialDate = new Date(finalDate)
+//     initialDate.setDate(initialDate.getDate() + 1)
+
+//     index++
+//     if(finalDate > finDate) 
+//         condition=true
+// }
+
+// for(let i=0; i<dateRanges.length; i++) {
+//     const newExplorerDaily = new ExploreTrendRequest()
+//     newExplorerDaily.addKeyword(keyword)
+//         .between(dateRanges[i].initialDate, dateRanges[i].finalDate)
+//         .download().then( file => {
+//             const fileToCSV = new objToCSV(file)
+//             return fileToCSV.toDisk(`./csv/daily-${dateRanges[i].initialDate}_to_${dateRanges[i].finalDate}.csv`)
+//         })
+// }
+
